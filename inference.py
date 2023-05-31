cffa27594c957b68ef58f0d77f0cc088ac400e1c
Kirk <kirk@Tarch.com>
2023-05-30 23:00:31 -0400

    fixing things

--- inference.py
+++ inference.py
@@ -8,60 +8,8 @@ from src.test_audio2coeff import Audio2Coeff
 from src.facerender.animate import AnimateFromCoeff
 from src.generate_batch import get_data
 from src.generate_facerender_batch import get_facerender_data
-import torch
-from time import strftime
-import os, sys, time
 
-from src.utils.preprocess import CropAndExtract
-from src.test_audio2coeff import Audio2Coeff  
-from src.facerender.animate import AnimateFromCoeff
-from src.generate_batch import get_data
-from src.generate_facerender_batch import get_facerender_data
-
-class Args:
-    def __init__(self, driven_audio='./examples/driven_audio/bus_chinese.wav', 
-                 source_image='./examples/source_image/full_body_2.png', 
-                 ref_eyeblink=None, ref_pose=None, 
-                 checkpoint_dir='./checkpoints', result_dir='./results', 
-                 pose_style=0, batch_size=2, expression_scale=1.0, 
-                 input_yaw=None, input_pitch=None, input_roll=None, 
-                 enhancer=None, background_enhancer=None, 
-                 cpu=False, face3dvis=False, still=False, 
-                 preprocess='crop', net_recon='resnet50', init_path=None, 
-                 use_last_fc=False, bfm_folder='./checkpoints/BFM_Fitting/', 
-                 bfm_model='BFM_model_front.mat', focal=1015.0, center=112.0, 
-                 camera_d=10.0, z_near=5.0, z_far=15.0):
-
-        self.driven_audio = driven_audio
-        self.source_image = source_image
-        self.ref_eyeblink = ref_eyeblink
-        self.ref_pose = ref_pose
-        self.checkpoint_dir = checkpoint_dir
-        self.result_dir = result_dir
-        self.pose_style = pose_style
-        self.batch_size = batch_size
-        self.expression_scale = expression_scale
-        self.input_yaw = input_yaw
-        self.input_pitch = input_pitch
-        self.input_roll = input_roll
-        self.enhancer = enhancer
-        self.background_enhancer = background_enhancer
-        self.cpu = cpu
-        self.face3dvis = face3dvis
-        self.still = still
-        self.preprocess = preprocess
-        self.net_recon = net_recon
-        self.init_path = init_path
-        self.use_last_fc = use_last_fc
-        self.bfm_folder = bfm_folder
-        self.bfm_model = bfm_model
-        self.focal = focal
-        self.center = center
-        self.camera_d = camera_d
-        self.z_near = z_near
-        self.z_far = z_far
-        
-def inference(args):
+def main(args):
     #torch.backends.cudnn.enabled = False
 
     pic_path = args.source_image
@@ -165,11 +113,46 @@ def inference(args):
                                 enhancer=args.enhancer, background_enhancer=args.background_enhancer, preprocess=args.preprocess)
     
 if __name__ == '__main__':
-    args = Args(source_image='new_image_path.png', cpu=True)
+
+    parser = ArgumentParser()  
+    parser.add_argument("--driven_audio", default='./examples/driven_audio/bus_chinese.wav', help="path to driven audio")
+    parser.add_argument("--source_image", default='./examples/source_image/full_body_2.png', help="path to source image")
+    parser.add_argument("--ref_eyeblink", default=None, help="path to reference video providing eye blinking")
+    parser.add_argument("--ref_pose", default=None, help="path to reference video providing pose")
+    parser.add_argument("--checkpoint_dir", default='./checkpoints', help="path to output")
+    parser.add_argument("--result_dir", default='./results', help="path to output")
+    parser.add_argument("--pose_style", type=int, default=0,  help="input pose style from [0, 46)")
+    parser.add_argument("--batch_size", type=int, default=2,  help="the batch size of facerender")
+    parser.add_argument("--expression_scale", type=float, default=1.,  help="the batch size of facerender")
+    parser.add_argument('--input_yaw', nargs='+', type=int, default=None, help="the input yaw degree of the user ")
+    parser.add_argument('--input_pitch', nargs='+', type=int, default=None, help="the input pitch degree of the user")
+    parser.add_argument('--input_roll', nargs='+', type=int, default=None, help="the input roll degree of the user")
+    parser.add_argument('--enhancer',  type=str, default=None, help="Face enhancer, [gfpgan, RestoreFormer]")
+    parser.add_argument('--background_enhancer',  type=str, default=None, help="background enhancer, [realesrgan]")
+    parser.add_argument("--cpu", dest="cpu", action="store_true") 
+    parser.add_argument("--face3dvis", action="store_true", help="generate 3d face and 3d landmarks") 
+    parser.add_argument("--still", action="store_true", help="can crop back to the original videos for the full body aniamtion") 
+    parser.add_argument("--preprocess", default='crop', choices=['crop', 'resize', 'full'], help="how to preprocess the images" ) 
+
+    # net structure and parameters
+    parser.add_argument('--net_recon', type=str, default='resnet50', choices=['resnet18', 'resnet34', 'resnet50'], help='useless')
+    parser.add_argument('--init_path', type=str, default=None, help='Useless')
+    parser.add_argument('--use_last_fc',default=False, help='zero initialize the last fc')
+    parser.add_argument('--bfm_folder', type=str, default='./checkpoints/BFM_Fitting/')
+    parser.add_argument('--bfm_model', type=str, default='BFM_model_front.mat', help='bfm model')
+
+    # default renderer parameters
+    parser.add_argument('--focal', type=float, default=1015.)
+    parser.add_argument('--center', type=float, default=112.)
+    parser.add_argument('--camera_d', type=float, default=10.)
+    parser.add_argument('--z_near', type=float, default=5.)
+    parser.add_argument('--z_far', type=float, default=15.)
+
+    args = parser.parse_args()
+
     if torch.cuda.is_available() and not args.cpu:
         args.device = "cuda"
     else:
         args.device = "cpu"
 
-    inference(args)
-
+    main(args)
